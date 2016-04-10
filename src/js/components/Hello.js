19fbfc66538e87b5d7b300069caabc45919a172c
Jon Welzel <jnwezel@gmail.com>
2016-04-09 23:31:01 -0300

    vou arrancar o storybook; tentando add watch de sass

--- src/js/components/Hello.js
+++ src/js/components/Hello.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
 class Hello extends React.Component {
+
     render() {
         return (
             <h1>Hello World</h1>
         )
     }
+
 }
 
 export default Hello
--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,8 @@ module.exports = {
         publicPath: 'http://localhost:8080/dist/'
     },
 
+    devtool: 'source-map',
+
     plugins: [
         new webpack.HotModuleReplacementPlugin()
     ],
