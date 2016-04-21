a106575ea5162452be044bc89e4d5d1aa1aebda5
Jonathan Welzel <jnwelzel@gmail.com>
2016-04-21 17:23:30 -0300

    landing page WIP

--- src/js/application.js
+++ src/js/application.js
@@ -2,6 +2,15 @@ import React from 'react'
 import ReactDom from 'react-dom'
 import Card from './components/Card'
 import Button from './components/Button'
+import {BLURRY_BEACH, DRACULA} from './constants/gradient_classes'
 
-ReactDom.render(<Card/>, document.getElementById('card-container'))
-ReactDom.render(<Button label="Button" icon={<i className="icon-github-alt"/>}/>, document.getElementById('btn-container'))
+
+const iconGithub = <i className="icon-github-alt" />
+ReactDom.render(<Button label="get started" className="ch-btn-white" icon={iconGithub}/>, document.getElementById('Button-get-started'))
+
+const card1Text = 'Sign up with your Github account'
+const card2Text = 'Your organizations become groups'
+const card3Text = 'And your repos become channels'
+ReactDom.render(<Card content={card1Text} />, document.getElementById('Card-1'))
+ReactDom.render(<Card content={card2Text} gradient={DRACULA} />, document.getElementById('Card-2'))
+ReactDom.render(<Card content={card3Text} gradient={BLURRY_BEACH} />, document.getElementById('Card-3'))
--- src/js/constants/gradient_classes.js
+++ src/js/constants/gradient_classes.js
@@ -3,3 +3,4 @@ const prefix = 'ch-gradient-'
 export const SHORE = `${prefix}shore`
 export const DRACULA = `${prefix}dracula`
 export const BLURRY_BEACH = `${prefix}blurry-beach`
+export const BACK_TO_EARTH = `${prefix}back-to-earth`
