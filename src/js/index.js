8cd687bab09dac9469cbe7862466d426d6b99e17
Jonathan Welzel <jnwelzel@gmail.com>
2016-05-13 15:46:48 -0300

    arquivos js separados para landing page e aplicacao

---
+++ src/js/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+import CardLandingPage from './components/CardLandingPage'
+import Button from './components/Button'
+import {BLURRY_BEACH, DRACULA} from './constants/gradient_classes'
+
+
+const iconGithub = <i className="icon-github-alt" />
+ReactDom.render(<Button label="get started" className="ch-btn white" icon={iconGithub}/>, document.getElementById('Button-get-started'))
+ReactDom.render(<Button label="get started" className="ch-btn" icon={iconGithub}/>, document.getElementById('Button-get-started-bottom'))
+
+const card1Text = 'Sign up with your Github account'
+const card2Text = 'Your organizations become groups'
+const card3Text = 'And your repos become channels'
+ReactDom.render(<CardLandingPage text={card1Text} number="1" icon="icon-github-square" />, document.getElementById('Card-1'))
+ReactDom.render(<CardLandingPage text={card2Text} number="2" gradient={DRACULA} icon="icon-share-alt" />, document.getElementById('Card-2'))
+ReactDom.render(<CardLandingPage text={card3Text} number="3" gradient={BLURRY_BEACH} icon="icon-comments" />, document.getElementById('Card-3'))
--- webpack.config.js
+++ webpack.config.js
@@ -2,41 +2,38 @@ var webpack = require('webpack')
 var path = require('path')
 
 module.exports = {
-    entry: [
-        'webpack-dev-server/client?http://localhost:8080',
-        'webpack/hot/only-dev-server',
-        './src/js/application'
-    ],
+  entry: {
+    application: ['webpack-dev-server/client?http://localhost:8080', 'webpack/hot/only-dev-server', './src/js/application'],
+    index: ['./src/js/index']
+  },
 
-    stats: {
-        colors: true,
-        reasons: true
-    },
+  stats: {
+    colors: true,
+    reasons: true
+  },
 
-    output: {
-        path: path.join(__dirname, '/dist/'),
-        filename: 'bundle.js',
-        publicPath: 'http://localhost:8080/dist/'
-    },
+  output: {
+    path: path.join(__dirname, '/dist/'),
+    filename: '[name].js',
+    publicPath: 'http://localhost:8080/dist/'
+  },
 
-    devtool: 'source-map',
+  devtool: 'source-map',
 
-    plugins: [
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.optimize.UglifyJsPlugin({
-            compress: {
-                warnings: false
-            }
-        })
-    ],
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    })
+  ],
 
-    module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loaders: ['react-hot', 'babel?presets[]=es2015&presets[]=react']
-            }
-        ]
-    }
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loaders: ['react-hot', 'babel?presets[]=es2015&presets[]=react']
+    }]
+  }
 }
